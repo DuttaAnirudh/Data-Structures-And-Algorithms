7bd8148569147845980cdf9785c0f6ac0fe45cb7
DuttaAnirudh <worktodutta@gmail.com>
2024-11-27 22:55:02 +0530

    working with sliding window pattern problems

--- Problem Solving Patterns/pattern.js
+++ Problem Solving Patterns/pattern.js
@@ -372,3 +372,106 @@ function isSubsequence2(str1, str2) {
 // console.log(isSubsequence2("sing", "sting")); // true
 // console.log(isSubsequence2("abc", "abracadabra")); // true
 // console.log(isSubsequence2("abc", "acb")); // false (order matters)
+
+function maxSubarraySum(arr, num) {
+  if (arr.length < num) return null;
+
+  let maxsum = 0;
+  let curSum = 0;
+
+  for (let i = 0; i < num; i++) {
+    maxsum += arr[i];
+  }
+
+  curSum = maxsum;
+
+  for (let i = num; i < arr.length; i++) {
+    curSum = curSum - arr[i - num] + arr[i];
+    maxsum = Math.max(maxsum, curSum);
+  }
+
+  return maxsum;
+}
+
+// console.log(maxSubarraySum([100, 200, 300, 400], 2)); // 700
+// console.log(maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4)); // 39
+// console.log(maxSubarraySum([-3, 4, 0, -2, 6, -1], 2)); // 5
+// console.log(maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1], 2)); // 5
+// console.log(maxSubarraySum([2, 3], 3)); // null
+
+function minSubArrayLen(arr, num) {
+  let curSum = 0; // current sum of the window
+  let start = 0; // start of sliding window
+  let end = 0; // end of sliding window
+  let minLen = Infinity; // min subarray(window) length
+
+  while (start < arr.length) {
+    // Expanding the current window
+    // if current window doesn't add up to the given 'num' then
+    // add the number to the curSum
+    // move the current window to right
+    if (curSum < num && end < arr.length) {
+      curSum += arr[end];
+      end++;
+    }
+    // Shrinking the current window
+    // if current window sum(curSum) adds up to at least the 'num' given
+    else if (curSum >= num) {
+      minLen = Math.min(minLen, end - start);
+      curSum -= arr[start];
+      start++;
+    }
+    // when current total is less than required total but we reach the end, we need the loop to stop or else we'll be in an infinite loop
+    else {
+      break;
+    }
+  }
+
+  return minLen === Infinity ? 0 : minLen;
+}
+
+// console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7)); // 2 -> because [4,3] is the smallest subarray
+// console.log(minSubArrayLen([2, 1, 6, 5, 4], 9)); // 2 -> because [5,4] is the smallest subarray
+// console.log(minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52)); // 1 -> because [62] is greater than 52
+// console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39)); // 3
+// console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55)); // 5
+// console.log(minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11)); // 2
+// console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95)); // 0
+
+function findLongestSubstring(str) {
+  let longest = 0;
+  let seen = {};
+  let start = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+
+    // console.log("CHAR", char);
+    // console.log(start);
+    // console.log("i :", i);
+
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+    }
+
+    // console.log(start);
+    // index - beginning of substring + 1 (to include current in count)
+    longest = Math.max(longest, i - start + 1);
+    console.log("LONGEST:", longest);
+    // store the index of the next char so as to not double count
+    seen[char] = i + 1;
+    // console.log(seen);
+    // console.log("-------");
+  }
+
+  return longest;
+}
+
+// console.log(findLongestSubstring("")); // 0
+// console.log(findLongestSubstring("abc")); // 3
+// console.log(findLongestSubstring("rithmschool")); // 7
+// console.log(findLongestSubstring("thisisawesome")); // 6
+// console.log(findLongestSubstring("thecatinthehat")); // 7
+// console.log(findLongestSubstring("bbbbbb")); // 1
+// console.log(findLongestSubstring("longestsubstring")); // 8
+// console.log(findLongestSubstring("thisishowwedoit")); // 6
