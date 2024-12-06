8e0b7448af9c510402cd2dcba0544b305843d000
DuttaAnirudh <worktodutta@gmail.com>
2024-12-06 21:17:03 +0530

    sortng arrays using insertion sort and selection sort approach

---
+++ Sort/insertionSort.js
@@ -0,0 +1,18 @@
+// 80. Insertion Sort
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentVal = arr[i];
+    let index = i;
+    for (let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+      index = j;
+      console.log(arr);
+    }
+    arr[index] = currentVal;
+    // console.log(arr);
+  }
+  return arr;
+}
+
+console.log([2, 1, 9, 76, 4]);
+console.log(insertionSort([2, 1, 9, 76, 4]));
